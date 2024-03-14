339aab1dedd90700e441afd005b49ef704ecbcbe
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-03-14 11:39:45 +0200

    added css for the valid and invalid

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function validateSyntax() {
+
     let input = document.getElementById('petInput').value;
+
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+
+    let result=""; // Placeholder for validation result
 
     // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+        // Check if input starts with 'pet' and followed by alphanumeric characters
+
+        let pattern = /^pet[0-9]{4}[a-zA-Z]+/;
+
+    // Check if the input matches the pattern
+
+        if(pattern.test(input)) {
+            result = 'Valid Syntax <span class="valid"></span>';  // Set result message for valid input
+
+        } else {
+            result = 'Invalid Syntax <span class="invalid"></span>';  // Set result message for invalid input
+        }
+
+
+
+    // The code is to display the validation result
+    document.getElementById('result').innerHTML = result;
 }
 
 
